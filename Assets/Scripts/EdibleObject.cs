7d62afcc17161cbccf6c5b0e19d9819287f0d9e5
Caleb Boyle <caleb.boyle@mail.utoronto.ca>
2022-09-17 14:11:13 -0400

    Add edible object script

---
+++ Assets/Scripts/EdibleObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdibleObject : MonoBehaviour
+{
+    [field: SerializeField] public int Score { get; set; }
+
+    [field: SerializeField] public float Size { get; set; }
+}
