c5413d3272ccba5b9132c59e68f424c8668b2a19
Caleb Boyle <caleb.boyle@mail.utoronto.ca>
2022-09-17 14:48:16 -0400

    Add score updating to collision

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,30 +6,32 @@ public class PlayerCollision : MonoBehaviour
 {
     public float mass;
     Rigidbody rb;
+    private ScoreManager scoreManager;
 
     void Start()
     {
 
-    rb = GetComponent<Rigidbody>();
-    mass = rb.mass;
-        
+        rb = GetComponent<Rigidbody>();
+        mass = rb.mass;
+        scoreManager = GameObject.FindObjectOfType<ScoreManager>();
     }
 
- void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "edible"){
+        if (collision.gameObject.tag == "edible")
+        {
             var edibleObject = collision.gameObject.GetComponent<EdibleObject>();
             float growthSize = edibleObject.Size;
 
             if (growthSize < mass)
             {
-                Destroy(collision.gameObject);
 
                 mass += growthSize;
+                scoreManager.UpdateScore(edibleObject.Score);
 
                 gameObject.transform.localScale += new Vector3(growthSize, growthSize, growthSize);
                 gameObject.transform.position += new Vector3(0, growthSize / 2, 0);
-
+                Destroy(collision.gameObject);
             }
         }
     }
